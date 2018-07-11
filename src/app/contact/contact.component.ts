447ca7810c6884d23c70fe7b0a06645344feb068
howdycrystalm <howdycrystalm@gmail.com>
2018-07-10 20:51:35 -0700

    end of day 3

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CONTACTS } from '../mock-contacts';
+import { Contact } from '../contact';
 
 @Component({
   selector: 'app-contact',
@@ -7,11 +8,24 @@ import { CONTACTS } from '../mock-contacts';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
-
+  newContact: Contact = new Contact();
   contacts = CONTACTS;
 
   constructor() { }
+  deleteContact(del) {
+    const remove: number = this.contacts.indexOf(del);
+    if (remove !== -1) {
+      this.contacts.splice(remove, 1);
+    }
+  }
 
+  addContact() {
+    const lastIndex = this.contacts.length - 1;
+    const last = this.contacts[lastIndex];
+    this.newContact.id = last.id + 1;
+    this.contacts.push(this.newContact);
+    this.newContact = new Contact();
+  }
   ngOnInit() {
   }
 
